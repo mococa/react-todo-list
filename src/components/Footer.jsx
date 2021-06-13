bac88c250cfd63931fc6c4747b6c009637478d5c
Padeiro a Fênix <mococs>
2021-06-13 05:17:30 -0300

    Added footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {FaGithub} from 'react-icons/fa'
 
 function Footer() {
     return (
         <footer>
-            
+           <a href="https://github.com/mococa" target="_blank">
+                <h3><FaGithub style={{width:'50px', height:'100%'}}/> Made by Luiz Felipe Moureau</h3> 
+            </a>
         </footer>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Header({fn}) {
     return (
-        <div className="header">
+        <header className="header">
             <h1>Todo List</h1>
             
             <button className="clickable" onClick={fn}>
                 Create Task
             </button>
-        </div>
+        </header>
     )
 }
 
